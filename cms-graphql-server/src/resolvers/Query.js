b5c1dfe297d4f483abf4a5b47fff7ea7519db871
Matt Browne <mbrowne@artnet.com>
2019-02-13 17:53:19 -0500

    added entityDefinitions query to graphql server

--- cms-graphql-server/src/resolvers/Query.js
+++ cms-graphql-server/src/resolvers/Query.js
@@ -1,6 +1,7 @@
-// import fs from 'fs'
 import path from 'path'
+import fsModule from 'fs'
 import config from '~/config'
+const fs = fsModule.promises
 
 const entityDefsDir = path.join(
     config.PROJECT_ROOT,
@@ -9,7 +10,34 @@ const entityDefsDir = path.join(
 )
 
 export const Query = {
-    async entityDefinition(_, { id }) {
-        return await import(path.join(entityDefsDir, id + '.json'))
+    async entityDefinitions(_, { page, pageSize }) {
+        if (page || pageSize) {
+            throw Error(
+                'entityDefinitions query does not yet support pagination'
+            )
+        }
+        const entityDefFiles = (await fs.readdir(entityDefsDir)).filter(
+            filename => filename.match(/\.json$/)
+        )
+        const results = await Promise.all(
+            entityDefFiles.map(async filename => {
+                const entityDefId = path.parse(filename).name
+                return await loadEntityDef(entityDefId)
+            })
+        )
+
+        return {
+            results,
+            totalCount: entityDefFiles.length,
+        }
     },
+
+    entityDefinition(_, { id }) {
+        return loadEntityDef(id)
+    },
+}
+
+// @TODO Do dynamic imports get cached? If so we might need to read the JSON file directly instead.
+async function loadEntityDef(id) {
+    return (await import(path.join(entityDefsDir, id + '.json'))).default
 }
