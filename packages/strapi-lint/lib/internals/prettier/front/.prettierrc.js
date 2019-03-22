057a8d89b0a60c3d46ab797a4140f1a77aaaef7d
Matt Browne <mbrowne@artnet.com>
2019-03-22 15:28:04 -0400

    strapi master at a42c2a483ca6e35a25c7d214bedd2e0cefc48dbb

---
+++ packages/strapi-lint/lib/internals/prettier/front/.prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * prettier configuration for front-end files
+ */
+
+module.exports = {
+  printWidth: 110,
+  useTabs: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+};
